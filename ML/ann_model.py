3f7e8ed63ad353b32b7f4a23e0209619fd5a3412
Tarun2605 <tarun.p.ug22@nsut.ac.in>
2025-02-10 17:14:34 +0530

    Docstrings

--- ML/ann_model.py
+++ ML/ann_model.py
@@ -15,37 +15,18 @@ import matplotlib.pyplot as plt
 
 class ANNModel(BaseModel):
     def __init__(self):
-        """
-        Constructor for Artificial Neural Network model
-        :param name: Name of the model
-        :param layers: List of layers in the model
-        :param loss_layer: Loss layer for the model
-        :param metric_layer: Metric layer for the model
-        :param learning_rate: Learning rate for the model
-        """
         super().__init__("Artificial Neural  Network")
         self._logger= CustomLogger(name="AnnClassLogger", 
                                    log_file="./Logs/layers.log").get_logger()
-        self._layers: List[Layer] = []
-        self._loss_layer: LossLayer = None
-        self._metric_layer: LossLayer =[]
         self._math_ops=MathOps()
-        self._learning_rate=None
+        self._loss_layer: LossLayer = None
+        self._learning_rate: float=None
+        self._batch_size: int=None
+        self._layers: List[Layer] = []
+        self._metric_layer: LossLayer =[]
     
     def add_layer(self, number_of_neurons: int, input_shape: int=1,
                   activation_function: str = "sigmoid", alpha: float =0.01, **kwargs) -> None:
-        """
-        Adds a new layer to the model.
-
-        Args:
-            number_of_neurons (int): The number of neurons in the new layer.
-            input_shape (int, optional): The input shape of the new layer. Defaults to 1.
-            activation_function (str, optional): The activation function for the new layer. Defaults to "sigmoid".
-            alpha (float, optional): The learning rate for the new layer. Defaults to 0.01.
-
-        Raises:
-            CustomException: If there is an error adding the layer.
-        """
         try:
             if len(self._layers)!=0:
                 input_shape: int=self._layers[-1].get_number_of_neurons()
@@ -60,18 +41,6 @@ class ANNModel(BaseModel):
             raise_custom_exception(CustomException, message=f"Error in add_layer: {e}")
 
     def _forward_propagation(self, X: np.ndarray, alpha: float= 0.1) -> np.ndarray:
-        """
-        Performs a forward pass through the model.
-
-        Args:
-            X (np.ndarray): Input to the model
-
-        Returns:
-            np.ndarray: Output of the model
-
-        Raises:
-            CustomException: If there is an error in the forward pass
-        """
         try:
             for layer in self._layers:
                 X = layer.forward(X, alpha=alpha)
@@ -84,16 +53,6 @@ class ANNModel(BaseModel):
             raise_custom_exception(CustomException, message=f"Error in forward propagation: {e}")
 
     def _back_propagation(self, d_output: np.ndarray, learning_rate: float, alpha: float=0.1) -> None:    
-        """
-        Performs a backward pass through the model.
-
-        Args:
-            d_output (np.ndarray): Error derivative of the output
-            learning_rate (float): Learning rate for the model
-
-        Raises:
-            CustomException: If there is an error in the backward pass
-        """
         try:
             for layer in reversed(self._layers):
                 d_output = layer.backward(d_output=d_output, learning_rate=learning_rate, alpha=alpha)
@@ -104,52 +63,48 @@ class ANNModel(BaseModel):
             self._logger.critical(f"Critical error in back propagation: {e}")
             raise_custom_exception(CustomException, message=f"Error in back propagation: {e}")
 
-    def train(self, X, Y, epochs: int=1000, learning_rate: float=0.001):
-        """
-        Trains the model using the given input and output data.
-
-        Args:
-            X (np.ndarray): Input data
-            Y (np.ndarray): Output data
-            epochs (int, optional): Number of epochs to train. Defaults to 1000.
-            learning_rate (float, optional): Learning rate for the model. Defaults to 0.001.
-
-        Returns:
-            List[float]: List of loss values for each epoch
-        """
-
+    def _single_iteration(self, X: np.float64, Y: np.float64, learning_rate: float=0.001, alpha: float=0.1) -> float:
+        try:
+            output = self._forward_propagation(X, alpha=alpha)
+            loss = self._loss_layer.forward(y_true=Y, y_pred=output)
+            d_output = self._loss_layer.derivative_loss(y_true=Y, y_pred=output)
+            self._back_propagation(d_output=d_output, learning_rate=learning_rate, alpha=alpha)
+            return loss
+        except CustomException as e:
+            self._logger.error(f"Error in single iteration: {e}")
+            raise_custom_exception(CustomException, message=f"Error in single iteration: {e}")
+        except Exception as e:
+            self._logger.critical(f"Critical error in single iteration: {e}")
+            raise_custom_exception(CustomException, message=f"Error in single iteration: {e}")
+    def train(self, X: np.float64, Y: np.float64, epochs: int=1000, learning_rate: float=0.001):
         losses = []
         for epoch in range(epochs):
-            output = self._forward_propagation(X)
+            # output = self._forward_propagation(X)
             
-            loss = self._loss_layer.forward(y_true=Y, y_pred=output)
-            losses.append(loss)
-            d_output = self._loss_layer.derivative_loss(y_true=Y, y_pred=output)
+            # loss = self._loss_layer.forward(y_true=Y, y_pred=output)
+            # losses.append(loss)
+            # d_output = self._loss_layer.derivative_loss(y_true=Y, y_pred=output)
 
-            self._back_propagation(d_output=d_output, learning_rate=learning_rate)
+            # self._back_propagation(d_output=d_output, learning_rate=learning_rate)
+            for idx in range(0, X.shape[0], self._batch_size):
+                X_batch = X[idx:idx+self._batch_size]
+                Y_batch = Y[idx:idx+self._batch_size]
+                loss = self._single_iteration(X=X_batch, Y=Y_batch, learning_rate=learning_rate, alpha=self._alpha)
+                losses.append(loss)
             if epoch % 500 == 0:
+                output = self._forward_propagation(X)
                 metric_loss=self._metric_layer.forward(y_true=Y, y_pred=output)
                 print(f"Epoch {epoch}, Loss: {loss:.6f}, metric: {metric_loss:.6f}")
 
         return losses
     
-    def compile(self, loss: str="mse", metric: str="mse", learning_rate: float=0.001):
-        """
-        Compiles the model with the given loss and metric.
-
-        Args:
-            loss (str, optional): Loss function to use. Defaults to "mse".
-            metric (str, optional): Metric to use. Defaults to "mse".
-            learning_rate (float, optional): Learning rate for the model. Defaults to 0.001.
-
-        Raises:
-            CustomException: If there is an error compiling the model.
-        """
-
+    def compile(self, loss: str="mse", metric: str="mse", learning_rate: float=0.001, alpha: float=0.1, batch_size: int=1):
         try:
             self._loss_layer = LossLayer(loss_type=loss)
             self._metric_layer = LossLayer(loss_type=metric)
             self._learning_rate = learning_rate
+            self._batch_size = batch_size
+            self._alpha = alpha
         except CustomException as e:
             self._logger.error(f"Error in compile: {e}")
             raise_custom_exception(CustomException, message=f"Error in compile: {e}")
@@ -158,32 +113,12 @@ class ANNModel(BaseModel):
             raise_custom_exception(CustomException, message=f"Error in compile: {e}")
     
     def predict(self, X):
-        """
-        Perform a forward pass through the model to make predictions.
-
-        Args:
-            X (np.ndarray): Input data for which predictions are to be made.
-
-        Returns:
-            np.ndarray: Predicted output data after passing through all layers.
-        """
-
         output = X
         for layer in self._layers:
             output = layer.forward(output)
         return output
     
     def evaluate(self, X, Y):
-        """
-        Evaluates the model on the given input data and true output data.
-
-        Args:
-            X (np.ndarray): Input data for evaluation.
-            Y (np.ndarray): True output data corresponding to the input data.
-
-        Returns:
-            Tuple[np.float64, np.float64]: The computed loss and metric values for the evaluation data.
-        """
 
         output = self.predict(X)
         loss = self._loss_layer.forward(y_true=Y, y_pred=output)
@@ -191,13 +126,6 @@ class ANNModel(BaseModel):
         return loss, metric
     
     def get_weights_biases(self):
-        """
-        Gets the weights and biases for all layers in the model.
-
-        Returns:
-            Tuple[List[np.ndarray], List[np.ndarray]]: A tuple of two lists, the first containing the weights for each layer and the second containing the biases for each layer in the model.
-
-        """
         weights = []
         biases = []
         for layer in self._layers:
@@ -229,7 +157,7 @@ model = ANNModel()
 model.add_layer(number_of_neurons=64, input_shape=X_train.shape[1], activation_function="relu")
 model.add_layer(number_of_neurons=32, activation_function="relu")
 model.add_layer(number_of_neurons=1, activation_function="linear")
-model.compile(loss="mse", metric="mae", learning_rate=0.001)
+model.compile(loss="mse", metric="mae", learning_rate=0.001, alpha=0.01, batch_size=100)
 
 # Train the model
 losses = model.train(X_train, Y_train)
