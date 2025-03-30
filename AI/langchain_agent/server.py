dd5509ceeda58f3df11cdd8a4106b7070daf1b6b
Tarun <tarunprakash2605@gmail.com>
2025-03-30 13:38:34 +0530

    Setting up fastapi for the langchain agent

--- AI/langchain_agent/server.py
+++ AI/langchain_agent/server.py
@@ -14,14 +14,15 @@ from dotenv import load_dotenv
 load_dotenv(override=True)
 llm = ChatGroq(model_name="llama3-8b-8192", api_key=os.getenv("GROQ_API_KEY"), async_client=True)
 embedding = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2")
-qa_system = MongoDBAtlasQA(MONGO_URI, DB_NAME, COLLECTION_NAME, embedding, INDEX_NAME, llm)
 
 app = FastAPI()
 
 class QueryRequest(BaseModel):
     query: str
+    conversation_id: str 
 
 @app.post("/query")
 async def query_mongo(request: QueryRequest):
+    qa_system = MongoDBAtlasQA(MONGO_URI, DB_NAME, COLLECTION_NAME, embedding, INDEX_NAME, llm, conversation_id=request.conversation_id)
     response = qa_system.query(request.query)
     return {"response": response}
