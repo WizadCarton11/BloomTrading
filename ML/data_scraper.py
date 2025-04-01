4b6e61b396862c35d450f537eb31ccd0cbcb8fca
Tarun <tarunprakash2605@gmail.com>
2025-04-01 15:11:55 +0530

    Commit before endsems

--- ML/data_scraper.py
+++ ML/data_scraper.py
@@ -3,25 +3,65 @@ from custom_exceptions import *
 from custom_logger import CustomLogger
 import requests
 import os
+# load env
+from dotenv import load_dotenv
+load_dotenv()
+import datetime
+import pandas as pd
 class StockDataScraper:
     """
     Class for scraping stock data from internet
     """
-    def __init__(self, stockSymbol, logger=None):
+    def __init__(self, stockSymbol):
         self.stock_symbol = stockSymbol
-        self.logger = logger
+        timestamp = datetime.datetime.now().strftime("%Y%m%d")
+        self.logger = CustomLogger("StockDataScraper", log_file=f'./Logs/stock_data_scraper/{timestamp}.log').get_logger()
 
+        self.stock_data= None
+        self.structured_stock_data= None
     
+    def dumpIntoCSV(self):
+        try: 
+            df = pd.DataFrame.from_dict(self.stock_data['Time Series (Daily)'], orient='index')
+
+            df.columns = ['Open', 'High', 'Low', 'Close', 'Volume']
+
+            # only save upto 1200 rows
+            df = df.iloc[:1200]
+            # Convert the columns to numeric
+            df = df.apply(pd.to_numeric) 
+            self.structured_stock_data = df
+
+            csv_path = os.path.join(os.getcwd(), 'stock_data', f'{self.stock_symbol}_data.csv')
+            # Check if the directory exists, if not create it
+            os.makedirs(os.path.dirname(csv_path), exist_ok=True)
+            # Save the DataFrame to a CSV file
+            df.to_csv(csv_path)
+
+            self.logger.info(f"Data dumped into {csv_path} successfully...")
+        except Exception as e:
+            self.logger.error(f"Failed to dump data into CSV: {e}")
+            raise CustomException(f"Failed to dump data into CSV: {e}")
     def getStockData(self, period='5y'):
         try:
             try:
+                # print("entering")
                 self.logger.info(f"Fetching stock data for {self.stock_symbol}...")
-                historical_data= self.yahoo_finance_data(period=period)
-            except Exception as e:
-                raise_custom_exception(ScraperError, message=f"Failed to fetch data for {self.stockSymbol}: {e}")
+                
+                # Yfinance is not working for some stocks, so we need to use Alpha Vantage
+                # historical_data= self.yahoo_finance_data(period=period)
 
-            historical_data.drop(['Dividends', 'Stock Splits'], axis=1, inplace=True)
-            
+                # ALPHAVANTAGE API
+                alpha_data = self.alpha_vantage_data()
+                historical_data=pd.DataFrame.from_dict(self.stock_data['Time Series (Daily)'], orient='index')
+                historical_data.columns = ['Open', 'High', 'Low', 'Close', 'Volume']
+                historical_data=historical_data.iloc[:1200].apply(pd.to_numeric)
+                # print(historical_data.size)
+            except Exception as e:
+                raise_custom_exception(ScraperError, message=f"Failed to fetch data for {self.stock_symbol}: {e}")
+
+            # used with yfinance ONLY
+            # historical_data.drop(['Dividends', 'Stock Splits'], axis=1, inplace=True)
             
             self.logger.info(f"Fetched stock data for {self.stock_symbol} successfully...")
             
@@ -64,9 +104,10 @@ class StockDataScraper:
 
     def alpha_vantage_data(self):
         try:
-            url=f"https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol={self.stock_symbol}&apikey={os.environ['ALPHA_VANTAGE_API_KEY']}"
+            url=f"https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol={self.stock_symbol}&apikey={os.environ['ALPHA_VANTAGE_API_KEY']}&outputsize=full&datatype=json"
             response = requests.get(url)
             data = response.json()
+            self.stock_data = data
             return data
         except Exception as e:
             raise CustomException(f"Failed to fetch data for {self.stock_symbol}: {e}")
--- ML/stock_data_analyser.py
+++ ML/stock_data_analyser.py
@@ -10,38 +10,76 @@ from custom_logger import CustomLogger
 import datetime
 from custom_exceptions import raise_custom_exception, ScraperError, CustomException
 
+import requests
+import os
+import pandas as pd
+import yfinance as yf
+import matplotlib.pyplot as plt
+import numpy as np
+from statsmodels.tsa.seasonal import seasonal_decompose
+from data_scraper import StockDataScraper
+from custom_logger import CustomLogger
+import datetime
+from custom_exceptions import raise_custom_exception, ScraperError, CustomException
+import plotly.graph_objects as go
+from sqlalchemy import create_engine
+from dotenv import load_dotenv
+load_dotenv()
 class StockDataAnalyser:
     """
     Class for analysing stock data
     """
     def __init__(self, stock_symbol):
-        self.stock_symbol = stock_symbol
+        self.stock_symbol: str = stock_symbol
         timestamp = datetime.datetime.now().strftime("%Y%m%d")
-        self.logger = CustomLogger("StockDataAnalyser", log_file=f'./ML/src/Logs/stock_data_analyser/{timestamp}.log').get_logger()
+        self.logger = CustomLogger("StockDataAnalyser", log_file=f'./Logs/stock_data_analyser/{timestamp}.log').get_logger()
+        self.stock_data=None
+
+
         
-    def fetch_and_plot_stock_data(self):
+    def fetch_from_db_and_analyze(self):
+        try:
+            self._fetch_from_sql()
+            self.plot_stock_data(self.stock_data)
+        except CustomException as e:
+            self.logger.error(f"A custom error occurred at StockDataAnalayser->fetch_from_sql_and_analyze: {e}")
+            return None
+        except Exception as e:
+            self.logger.error(f"An error occurred at StockDataAnalayser->fetch_from_sql_and_analyze: {e}")
+            return None        
+
+    def fetch_and_store_stock_data(self, mode='csv'):
         try:
             
             self.logger.info(f"Fetching stock data for {self.stock_symbol}...")
             
             try:
-                scraper = StockDataScraper(self.stock_symbol, logger=self.logger)
+                scraper = StockDataScraper(stockSymbol=self.stock_symbol)
                 historical_data = scraper.getStockData()
             except Exception as e:
                 raise_custom_exception(ScraperError, message=f"Failed to create StockDataScraper object for {self.stock_symbol}: {e}")
 
-            
-            historical_data = self.get_bollinger_bands(historical_data)
+            historical_data = self._get_bollinger_bands(historical_data)
             
             for i in range(1, 20):
                 historical_data[f'Close_Lag{i}'] = historical_data['Close'].shift(i)
             
             historical_data.drop(historical_data.head(20).index, inplace=True)
+            historical_data['Trend']=self.getSeasonalDecomposition(historical_data)
+            historical_data['Stock_name']=self.stock_symbol
             
-            # self.plot_stock_data(historical_data)
+            self.stock_data = historical_data
             
-            historical_data.to_csv(f'{self.stock_symbol}_data.csv')
-            self.logger.info(f"Saved stock data to CSV file: {self.stock_symbol}_data.csv")
+            if mode=='sql':
+                self._dump_into_sql()
+                self.logger.info(f"Saved stock data to sql")
+            else:
+                historical_data.to_csv(f'./stock_data/{self.stock_symbol}_data.csv')
+                self.logger.info(f"Saved stock data to CSV file: {self.stock_symbol}_data.csv")
+            
+            # For dev purposes only
+            # historical_data.to_csv(f'./stock_data/{self.stock_symbol}_data.csv')
+            # self.logger.info(f"Saved stock data to CSV file: {self.stock_symbol}_data.csv")
             
             return historical_data
         
@@ -51,22 +89,87 @@ class StockDataAnalyser:
         except Exception as e:
             self.logger.error(f"An error occurred at StockDataAnalayser->fetch_and_plot_stock_data: {e}")
             return None
-    
+
+    def fetch_and_plot_stock_data(self):
+        try:
+            
+            self.logger.info(f"Fetching stock data for {self.stock_symbol}...")
+            
+            try:
+                scraper = StockDataScraper(stockSymbol=self.stock_symbol)
+                historical_data = scraper.getStockData()
+            except Exception as e:
+                raise_custom_exception(ScraperError, message=f"Failed to create StockDataScraper object for {self.stock_symbol}: {e}")
+
+            historical_data = self._get_bollinger_bands(historical_data)
+            
+            for i in range(1, 20):
+                historical_data[f'Close_Lag{i}'] = historical_data['Close'].shift(i)
+            
+            historical_data.drop(historical_data.head(20).index, inplace=True)
+            historical_data['Trend']=self.getSeasonalDecomposition(historical_data)
+            historical_data['Stock_name']=self.stock_symbol
+            self.plot_stock_data(historical_data)
+            self.stock_data = historical_data
+            
+            
+            return historical_data
+        
+        except CustomException as e:
+            self.logger.error(f"A custom error occurred at StockDataAnalayser->fetch_and_plot_stock_data: {e}")
+            return None
+        except Exception as e:
+            self.logger.error(f"An error occurred at StockDataAnalayser->fetch_and_plot_stock_data: {e}")
+            return None
+
+    def _fetch_from_sql(self):
+        try:
+            db_url = os.getenv('POSTGRES_URI')
+
+            # Create SQLAlchemy engine
+            engine = create_engine(db_url)
+            df=pd.read_sql(f"SELECT * FROM {self.stock_symbol.lower()}", engine)
+            # df.index=df['']
+
+        except CustomException as e:
+            self.logger.error(f"A custom error occurred at StockDataAnalayser->_fetch_from_sql: {e}")
+            return None
+        except Exception as e:
+            self.logger.error(f"An error occurred at StockDataAnalayser->_fetch_from_sql: {e}")
+            return None
+
+    def _dump_into_sql(self):
+        try:
+            db_url = os.getenv('POSTGRES_URI')
+
+            # Create SQLAlchemy engine
+            engine = create_engine(db_url)
+            self.stock_data.to_sql(self.stock_symbol.lower(),engine, if_exists="replace", index=True)
+
+        except CustomException as e:
+            self.logger.error(f"A custom error occurred at StockDataAnalayser->dump_into_sql: {e}")
+            return None
+        except Exception as e:
+            self.logger.error(f"An error occurred at StockDataAnalayser->dump_into_sql: {e}")
+            return None
+
+
     def getSeasonalDecomposition(self, stock_data):
         """
         Perform seasonal decomposition on stock data.
         """
-        result = seasonal_decompose(stock_data['Close'], model='additive', period=1)
-        return result
+        result = seasonal_decompose(stock_data['Close'], model='additive', period=25)
+        return result.trend
     
     
 
-    def get_bollinger_bands(self,data, bollinger_window=20):
+    def _get_bollinger_bands(self,data, bollinger_window=20, short_window=20):
         """
         Calculate Bollinger Bands for a given stock data.
         """
         rolling_mean = data['Close'].rolling(window=bollinger_window).mean()
         rolling_std = data['Close'].rolling(window=bollinger_window).std()
+        data['EMA'] = data['Close'].ewm(span=short_window, adjust=False).mean()
         data['Bollinger_Upper'] = rolling_mean + (2 * rolling_std)
         data['Bollinger_Lower'] = rolling_mean - (2 * rolling_std)
         return data
@@ -75,14 +178,27 @@ class StockDataAnalyser:
         """
         Plot stock data with Bollinger Bands.
         """
-        plt.figure(figsize=(12, 6))
-        plt.plot(data['Close'], label='Close Price', color='blue')
-        plt.plot(data['Bollinger_Upper'], label='Bollinger Upper Band', color='red')
-        plt.plot(data['Bollinger_Lower'], label='Bollinger Lower Band', color='green')
-        plt.title(f'{self.stock_symbol} Bollinger Bands')
-        plt.xlabel('Date')
-        plt.ylabel('Price')
-        plt.legend()
-        plt.grid(True)
-        plt.tight_layout()
-        plt.show()
+        fig = go.Figure()
+
+        # Add Close Price line
+        fig.add_trace(go.Scatter(x=data.index, y=data['Close'], mode='lines', name='Close Price', line=dict(color='blue')))
+
+        # Add Bollinger Upper Band
+        fig.add_trace(go.Scatter(x=data.index, y=data['Bollinger_Upper'], mode='lines', name='Bollinger Upper Band', line=dict(color='red')))
+
+        # Add Bollinger Lower Band
+        fig.add_trace(go.Scatter(x=data.index, y=data['Bollinger_Lower'], mode='lines', name='Bollinger Lower Band', line=dict(color='green')))
+
+        # Add seasonal decomposition components
+        fig.add_trace(go.Scatter(x=data.index, y=data['EMA'], mode='lines', name='EMA', line=dict(color='orange')))
+        # Customize layout
+        fig.update_layout(
+            title="Stock Price with Bollinger Bands",
+            xaxis_title="Date",
+            yaxis_title="Price",
+            legend=dict(x=0, y=1),
+            # xaxis=dict(rangeslider=dict(visible=True))  # Enables zooming and panning
+        )
+
+        # Show the figure
+        fig.show()
